2529e326b468e19c184d08ac70947d6c12c78ec5
Johan Groth <jgroth83@gmail.com>
2020-08-11 17:09:19 +0200

    fix: export type

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export * from './components';
 import '@stencil/router';
+import { KompendiumConfig } from './types';
+export declare const kompendium: (
+    config?: Partial<KompendiumConfig>
+) => ((docs: any) => Promise<void>) | (() => any);
