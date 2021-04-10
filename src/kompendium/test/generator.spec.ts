320205653e79621fd2cc4bc4978b9bc751cee34c
Johan Groth <jgroth83@gmail.com>
2021-04-10 11:37:10 +0200

    fix(generator): use Stencil logger and not console

--- src/kompendium/test/generator.spec.ts
+++ src/kompendium/test/generator.spec.ts
@@ -1,16 +1,28 @@
 import tmp from 'tmp';
 import fs from 'fs';
 import { kompendiumGenerator } from '../generator';
+import { Config } from '@stencil/core/internal';
 
 describe('kompendium()', () => {
     let path: string;
     let tmpObject: any;
     let kompendium;
+    let stencilConfig: Config;
 
     beforeEach(() => {
         tmpObject = tmp.dirSync();
         path = tmpObject.name;
         kompendium = kompendiumGenerator({ path });
+        stencilConfig = {
+            logger: {
+                createTimeSpan: () => ({
+                    finish: () => null,
+                    duration: () => null,
+                }),
+                debug: () => null,
+                warn: () => null,
+            } as any,
+        };
     });
 
     afterEach(() => {
@@ -18,12 +30,12 @@ describe('kompendium()', () => {
     });
 
     it('should write the kompendium.json file to the configured path', async () => {
-        await kompendium({});
+        await kompendium({}, stencilConfig);
         expect(fs.existsSync(`${path}/kompendium.json`)).toBeTruthy();
     });
 
     it('should write the JSON data to the kompendium.json file', async () => {
-        await kompendium({ foo: 'bar' });
+        await kompendium({ foo: 'bar' }, stencilConfig);
         const json = fs.readFileSync(`${path}/kompendium.json`, 'utf8');
         const data = JSON.parse(json);
         expect(typeof data).toEqual('object');
