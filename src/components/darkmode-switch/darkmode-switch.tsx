2a4147711066ee805997362c409ad0100fb58647
Adrian Schmidt <adrian.schmidt@lime.tech>
2021-05-20 13:12:04 +0200

    feat: add config for light- and dark-mode
    Co-authored-by: Kiarokh Moattar <kiarokh@icloud.com>

---
+++ src/components/darkmode-switch/darkmode-switch.tsx
@@ -0,0 +1,128 @@
+import { Component, h, State } from '@stencil/core';
+
+const DEFAULT: Theme = 'system-default';
+const LIGHT: Theme = 'force-light';
+const DARK: Theme = 'force-dark';
+const CHECKBOX_LIGHT = false;
+const CHECKBOX_DARK = true;
+const THEME_EVENT_NAME = 'kompendium-theme';
+const LOCALSTORAGE_KEY = 'kompendium-theme';
+
+/**
+ * @private
+ */
+@Component({
+    tag: 'kompendium-darkmode-switch',
+    styleUrl: 'darkmode-switch.scss',
+    shadow: true,
+})
+export class DarkmodeSwitch {
+    @State()
+    private theme: Theme = 'system-default';
+
+    @State()
+    private systemSettingIsDark: boolean;
+
+    private checkbox: HTMLInputElement;
+    private colorSchemeMediaQuery: MediaQueryList;
+
+    constructor() {
+        const colorSchemeMediaQueryFallback = {
+            addEventListener: () => {},
+            matches: false,
+        } as unknown as MediaQueryList;
+
+        this.colorSchemeMediaQuery =
+            (window.matchMedia &&
+                window.matchMedia('(prefers-color-scheme: dark)')) ||
+            colorSchemeMediaQueryFallback;
+    }
+
+    public connectedCallback() {
+        this.colorSchemeMediaQuery.addEventListener(
+            'change',
+            this.handleSystemThemeChange
+        );
+    }
+
+    public disconnectedCallback() {
+        this.colorSchemeMediaQuery.removeEventListener(
+            'change',
+            this.handleSystemThemeChange
+        );
+    }
+
+    public componentWillLoad(): void {
+        this.systemSettingIsDark = this.colorSchemeMediaQuery.matches;
+
+        this.setTheme(
+            (localStorage.getItem(LOCALSTORAGE_KEY) || DEFAULT) as Theme
+        );
+    }
+
+    public render(): HTMLElement {
+        const props = {
+            checked:
+                this.theme === DARK ||
+                (this.theme === DEFAULT && this.systemSettingIsDark),
+        };
+
+        return (
+            <div class="mode-toggle">
+                <input
+                    type="checkbox"
+                    onChange={this.handleThemeChange}
+                    ref={this.getSelectRef}
+                    {...props}
+                />
+                <div class="mode-visualization">
+                    <div class="circle"></div>
+                    <div class="ray one"></div>
+                    <div class="ray two"></div>
+                    <div class="ray three"></div>
+                    <div class="ray four"></div>
+                </div>
+            </div>
+        );
+    }
+
+    private getSelectRef = (element: HTMLInputElement) => {
+        this.checkbox = element;
+    };
+
+    private handleSystemThemeChange = (e: MediaQueryListEvent) => {
+        this.systemSettingIsDark = !!e.matches;
+        if (this.theme === DEFAULT) {
+            this.checkbox.checked = !this.checkbox.checked;
+        }
+    };
+
+    private handleThemeChange = () => {
+        const checkboxValue = !!this.checkbox.checked;
+
+        let newTheme: Theme = DEFAULT;
+
+        if (this.systemSettingIsDark) {
+            if (checkboxValue === CHECKBOX_LIGHT) {
+                newTheme = LIGHT;
+            }
+        } else {
+            if (checkboxValue === CHECKBOX_DARK) {
+                newTheme = DARK;
+            }
+        }
+
+        this.setTheme(newTheme);
+        document.dispatchEvent(
+            new CustomEvent<Theme>(THEME_EVENT_NAME, { detail: newTheme })
+        );
+    };
+
+    private setTheme = (value: Theme) => {
+        this.theme = value;
+        document.querySelector('html').dataset.theme = value;
+        localStorage.setItem(LOCALSTORAGE_KEY, value);
+    };
+}
+
+export type Theme = 'system-default' | 'force-light' | 'force-dark';
