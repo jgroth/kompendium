3cace527933ed61610842edcf8579159d9aa982e
Adrian Schmidt <adrian.schmidt@lime.tech>
2021-06-14 14:33:42 +0200

    fix(nav): make the chevron visible in dark mode too

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop, State, getAssetPath } from '@stencil/core';
+import { Component, h, Prop, State } from '@stencil/core';
 import { MenuItem } from '../../types';
 
 /**
@@ -138,9 +138,6 @@ export class Navigation {
             active: this.isRouteActive(item.path),
             'has-children': !!chapters.length,
         };
-        const path = getAssetPath(
-            '../collection/assets/icons/arrow-right-s-line.svg'
-        );
         const anchorAdditionalProps: any = {};
         if (!chapters.length) {
             anchorAdditionalProps.onClick = this.toggleMenu;
@@ -153,7 +150,20 @@ export class Navigation {
                     href={'#' + item.path}
                     {...anchorAdditionalProps}
                 >
-                    <img src={path} />
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        width="24"
+                        height="24"
+                    >
+                        <path fill="none" d="M0 0h24v24H0z" />
+                        <path
+                            id="arrow"
+                            d="M13.172 12l-4.95-4.95 1.414-1.414L16 12l-6.364 6.364-1.414-1.414z"
+                            fill="currentColor"
+                        />
+                    </svg>
+
                     <span class="link-text">{item.title}</span>
                 </a>
                 <ul class={classList}>{chapters.map(this.renderMenuItem)}</ul>
