1a6bf502cece32cc4f717c4c42171c726ee6f38c
Kiarokh Moattar <kiarokh@icloud.com>
2024-03-26 13:19:27 +0100

    fix(taglist): better visualize component tags

--- src/components/taglist/taglist.tsx
+++ src/components/taglist/taglist.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop, getAssetPath } from '@stencil/core';
+import { Component, h, Prop } from '@stencil/core';
 import { JsonDocsTag } from '@stencil/core/internal';
 
 /**
@@ -30,15 +30,17 @@ export class Taglist {
         const classList = {
             'tag-list': true,
         };
-        const path = getAssetPath(
-            '../collection/assets/icons/bookmark-fill.svg'
-        );
 
         classList[`tag--${tag.name}`] = true;
 
         return (
             <div class={classList}>
-                <img src={path} />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path
+                        d="M5 2h14a1 1 0 0 1 1 1v19.143a.5.5 0 0 1-.766.424L12 18.03l-7.234 4.536A.5.5 0 0 1 4 22.143V3a1 1 0 0 1 1-1z"
+                        fill="currentColor"
+                    ></path>
+                </svg>
                 <code>@{tag.name}</code>
                 <kompendium-markdown text={tag.text} />
             </div>
