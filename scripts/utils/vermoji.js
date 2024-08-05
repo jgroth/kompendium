f612d0fe34f1a77aa570693b5183e4c3cd016d4f
Adrian Schmidt <adrian.schmidt@lime.tech>
2024-08-05 15:43:56 +0200

    chore: remove redundant eslint-disable comment

--- scripts/utils/vermoji.js
+++ scripts/utils/vermoji.js
@@ -5,7 +5,6 @@ const VERMOJIS = require('./emojis');
 module.exports = function getVermoji(changelogPath) {
     const changelog = fs.readFileSync(changelogPath, 'utf8');
 
-    // eslint-disable-next-line no-constant-condition
     while (true) {
         const vermoji =
             VERMOJIS[Math.round(Math.random() * (VERMOJIS.length - 1))];
